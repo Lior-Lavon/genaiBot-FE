45d61bba3073848b86919acf4eecc03f2656703c
Lior Lavon <liorlavon554@gmail.com>
2025-04-03 00:31:56 +0200

    1

--- src/features/dashboard/dashboardSlice.js
+++ src/features/dashboard/dashboardSlice.js
@@ -43,7 +43,7 @@ export const testFunc = createAsyncThunk(
     console.log("folders : ", folders);
 
     // const url = `/testfunc?folders=${folders.Customer_Folder},${folders.Product_Folder},${folders.Category_Folder}`;
-    const url = `/testfunc?folders=Lior,lavon,eva`;
+    const url = `/testfunc?folders=${folders.Customer_Folder},lavon,eva`;
 
     return fetchMappingThunk(url, thunkAPI);
   }

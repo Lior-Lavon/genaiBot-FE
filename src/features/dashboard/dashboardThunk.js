6eec44773a757ccc3381170111cd6ffb8894ad90
Lior Lavon <liorlavon554@gmail.com>
2025-04-02 20:29:41 +0200

    1

--- src/features/dashboard/dashboardThunk.js
+++ src/features/dashboard/dashboardThunk.js
@@ -1,6 +1,8 @@
 import customFetch from "../../utills/customFetch";
 
 export const fetchMappingThunk = async (url, thunkAPI) => {
+  console.log("fetchMappingThunk url : ", url);
+
   try {
     const resp = await customFetch.get(url);
     return resp.data;
@@ -10,6 +12,8 @@ export const fetchMappingThunk = async (url, thunkAPI) => {
 };
 
 export const cacheDataThunk = async (url, body, thunkAPI) => {
+  console.log("cacheDataThunk url : ", url);
+
   try {
     const resp = await customFetch.post(url, body);
     return resp.data;

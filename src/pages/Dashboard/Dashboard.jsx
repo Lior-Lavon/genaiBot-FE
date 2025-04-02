bbd1be481fc7d1447750158e83c0a300c4e87b45
Lior Lavon <liorlavon554@gmail.com>
2025-04-02 23:36:31 +0200

    1

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -43,7 +43,7 @@ const Dashboard = () => {
     calculateContentWidth();
 
     // Load the BotMapping
-    dispatch(fetchMapping());
+    // dispatch(fetchMapping());
     console.log("calling testFunc");
     dispatch(testFunc());
 

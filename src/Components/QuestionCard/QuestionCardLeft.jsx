0a6dc92be44a6051c00f2e73480ad2d53fd50cb1
Lior Lavon <liorlavon554@gmail.com>
2025-04-24 17:54:44 +0200

    1

--- src/Components/QuestionCard/QuestionCardLeft.jsx
+++ src/Components/QuestionCard/QuestionCardLeft.jsx
@@ -430,6 +430,7 @@ const QuestionCardLeft = ({ chatItem, leftWidth }) => {
                   Proceed
                 </button>
               </div>
+              <p>lior</p>
             </div>
           )
         ))}
